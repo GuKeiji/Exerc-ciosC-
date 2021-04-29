8e90063b0e18df328adb1ed34e176a3dcc09e2c6
Gustavo Keiji <gugu.keiji.2004@gmail.com>
2021-04-29 19:53:07 -0300

    Desafio concluído

---
+++ Desafio/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 0;
+            int vezes = 0;
+
+            while (idade < 5)
+            {
+
+                if (vezes >= 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Insira sua idade novamente");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.WriteLine("\nInsira sua idade");
+                }
+
+                idade = int.Parse(Console.ReadLine());
+
+                if (idade < 5)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nIdade inválida\n");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    vezes++;
+                }
+
+            }
+
+            if (idade >= 5 && idade <= 7)
+            {
+                Console.WriteLine("\nVocê está na categoria Infantil A");
+            }
+            else
+            {
+                if (idade >= 8 && idade <= 10)
+                {
+                    Console.WriteLine("\nVocê está na categoria Infantil B");
+                }
+                else
+                {
+                    if (idade >= 11 && idade <= 13)
+                    {
+                        Console.WriteLine("\nVocê está na categoria Juvenil A");
+                    }
+                    else
+                    {
+                        if (idade >= 14 && idade <= 17)
+                        {
+                            Console.WriteLine("\nVocê está na categoria Juvenil B");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nVocê está na categoria Sênior");
+                        }
+                    }
+                }
+            }
+
+
+        }
+    }
+}
