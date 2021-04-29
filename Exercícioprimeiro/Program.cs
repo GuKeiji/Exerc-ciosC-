cc50305937bdb947605b0b54364dd609bb57b288
Gustavo Keiji <gugu.keiji.2004@gmail.com>
2021-04-29 18:33:42 -0300

    Exercícios 1 e 2 concuídos

---
+++ Exercícioprimeiro/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Exercícioprimeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 0;
+            int meses = 0;
+            int dias = 0;
+            int horas = 0;
+            int minutos = 0;
+            bool check = true;
+            int vezes = 0;
+            string texto = "";
+
+            while (check == true)
+            {
+                if (vezes >= 1)
+                {
+                    texto = " novamente";
+                }
+
+                Console.WriteLine("Insira sua idade" + texto);
+                idade = int.Parse(Console.ReadLine());
+
+                if (idade < 150)
+                {
+                    meses = idade * 12;
+                    dias = idade * 365;
+                    horas = idade * 8760;
+                    minutos = horas * 60;
+                    Console.WriteLine("\nMeses: " + meses);
+                    Console.WriteLine("Dias: " + dias);
+                    Console.WriteLine("Horas: " + horas);
+                    Console.WriteLine("Minutos: " + minutos);
+                    check = false;
+
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Desculpe, mas você é um difunto!!!\n");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    vezes++;
+                }
+
+
+
+            }
+        }
+    }
+}
---
+++ Exercíciosegundo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercíciosegundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anoNascimento;
+            int anoAtual;
+            int idade;
+            double semanas;
+
+            Console.WriteLine("\nInsira o ano em que nasceu");
+            anoNascimento = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nInsira o ano atual");
+            anoAtual = int.Parse(Console.ReadLine());
+
+            idade = anoAtual - anoNascimento;
+            semanas = idade * 52.143;
+
+            Console.WriteLine("\nSua idade é: " + idade);
+            Console.WriteLine("\nSua idade em semanas é aproximadamente: " + semanas.ToString("0"));
+        }
+    }
+}
